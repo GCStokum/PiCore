69070ca8e75feee2b5ce712b4f9a5c4870326a16
G. Carter Stokum <GCStokum@gmail.com>
2013-11-21 15:45:59 -0800

    Update to Major Loop Restructure
    Clock Cycles are handled now, and a "Rest" bit is now used to allow
    lower total CPU cycles.

--- Core_0v0v011.py
+++ Core_0v0v011.py
@@ -1,5 +1,5 @@
                                                                                #
-#v.0.1.001
+#v.0.0.011
 # Hello, This is an entry into my own Python based, RPi intended (to start with
 # open and free game framework
 # and will probably be a start point for a colaborative series.
@@ -13,15 +13,15 @@ import pygame
 import random
 import Colors as rgb
 
-# Primary Engine Bits
-clock = pygame.time.Clock()
 running = True
 myMousePos = myMouseClick = (0, 0)
+dir = 1
+
 screenWidth = 800
 screenHeight = 600
+
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
-# Secondary 
 topLeft = (1, 1)
 bottomLeft = (1 , screenHeight - 1)
 topRight = (screenWidth - 1, 1)
@@ -32,12 +32,11 @@ bgColour = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 random.seed()
 
 
-# Let's start doing some stuff
 print('Hello.')
 while running:
 
     ''' INPUT '''
-    for event in pygame.event.get():  # Poll only on event instead of constantly
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print('Goodbye!')
             running = False
@@ -49,14 +48,23 @@ while running:
             print('Mouse @', event.pos)
             myMousePos = event.pos
         else:
-            print (event.type)  # Whatelse is going on here?
-
-    clock.tick(20)  # Let our CPU rest and do other things for a few cycles
+            print (event.type)
+    '''
+    "Old Event Loop"
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        print('Goodbye!')
+        running = False
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        print ("Mouse Click @" , event.pos)
+        myMouseClick = event.pos
+    elif event.type == pygame.MOUSEMOTION:
+        print ("Mouse @ " , event.pos)
+        myMousePos = event.pos
+    '''
 
     
-    ''' LOGIC '''
-    
-    ''' DISPLAY / OUTPUT '''
+    ''' DISPLAY '''
     screen.fill(bgColour)
 
     pygame.draw.line(screen, (rgb.red), (bottomLeft), (myMousePos))
@@ -64,3 +72,34 @@ while running:
     pygame.draw.line(screen, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), (screenWidth / 2, screenHeight), (myMouseClick))
 
     pygame.display.flip()
+    
+'''
+import pygame
+y = 0
+dir = 1
+running = 1
+barheight = 124
+screen = pygame.display.set_mode((800, 600));
+
+barcolor = []
+for i in range(1, 63):
+    barcolor.append((0, 0, i*4))
+for i in range(1, 63):
+    barcolor.append((0, 0, 255 - i*4))
+
+while running:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = 0
+
+    screen.fill((0, 0, 0))
+    for i in range(0, barheight):
+        pygame.draw.line(screen, barcolor[i], (0, y+i), (799, y+i))
+
+    y += dir
+    if y + barheight > 599 or y < 0:
+        dir *= -1
+
+    pygame.display.flip()
+'''
+
