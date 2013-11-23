307082c7a10518582e672e89e47d8992f695b02a
G. Carter Stokum <GCStokum@gmail.com>
2013-11-23 13:10:59 -0800

    Text Blitting and Debug display

--- Core.py
+++ Core.py
@@ -1,5 +1,5 @@
                                                                                #
-#v.0.1.012
+#v.0.1.015
 # Hello, This is an entry into my own Python based, RPi intended (to start with
 # open and free game framework
 # and will probably be a start point for a colaborative series.
@@ -15,12 +15,17 @@ import Colors as rgb
 import Vectors as v
 
 # Primary Engine Bits
+Debug = True
 running = True
 myMousePos = myMouseClick = (0, 0)
 screenWidth = 800
 screenHeight = 600
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
+
+pygame.font.init()  # Needed if no pygame.init() safe to call multiple times
+# Select the font to use. Default font, 25 pt size.
+font = pygame.font.Font(None, 25)
 
 # Secondary
 Up = (0, -1)
@@ -33,7 +38,7 @@ bottomLeft = (1 , screenHeight - 1)
 topRight = (screenWidth - 1, 1)
 bottomRight = (screenWidth - 1, screenHeight -1)
 
-bgColour = (0, 127, r.randint(0,255))
+bgColour = (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255))
 
 r.seed()
 
@@ -52,7 +57,6 @@ class entity:
     def die(self):
         print('Goodbye Cruel World!')
 
-
 def kill(e):
     if e is entity:
         e.die()
@@ -61,7 +65,7 @@ def kill(e):
 print('Hello.')
 while running:
 
-    ''' INPUT / CONTROL '''
+    ''' INPUT / CONTROL Below '''
     for event in pygame.event.get():  # Poll only on event instead of constantly
         if event.type == pygame.QUIT:
             print('Goodbye!')
@@ -86,21 +90,42 @@ while running:
             print (event.type)  # Whatelse is going on here?
 
     clock.tick(250)  # Let our CPU rest and do other things for a few cycles
+    ''' INPUT / CONTROL Above '''
 
     
-    ''' LOGIC '''
+    ''' LOGIC Below '''
+
+    ''' LOGIC Above '''
 
     
-    ''' DISPLAY / OUTPUT '''
+    ''' DISPLAY / OUTPUT Below '''
     screen.fill(bgColour)
 
+    '''
+    # Set Pixel Color at point
     x = r.randint(0, screenWidth-1)
     y = r.randint(0, screenHeight-1)
     screen.set_at((x, y), (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255)))
+    '''
+
+    pygame.draw.line(screen, (rgb.red), (bottomLeft), (myMousePos),2)
+    pygame.draw.line(screen, (rgb.blue), (bottomRight), (myMousePos),2)
+    pygame.draw.line(screen, (r.randint(0,255),r.randint(0,255),r.randint(0,255)), (screenWidth / 2, screenHeight), (myMouseClick),3)
+
+    if Debug == True:
+        # Render the text. "True" means anti-aliased text. 
+        # Black is the color. This creates an image of the 
+        # letters, but does not put it on the screen
+        text = font.render("Pointer @ " + str(myMousePos), True, rgb.yellow)
+        # this is how to concatinate string bits :
+        # text = font.render("Score: "+str(score),True,black)
+
+        # Put the image of the text on the screen at 250x250
+        screen.blit(text, [5,5])
     
+        text = font.render("Clicked @ " + str(myMouseClick), True, rgb.yellow)
+        screen.blit(text, [5,30])
 
-    pygame.draw.line(screen, (rgb.red), (bottomLeft), (myMousePos))
-    pygame.draw.line(screen, (rgb.blue), (bottomRight), (myMousePos))
-    pygame.draw.line(screen, (r.randint(0,255),r.randint(0,255),r.randint(0,255)), (screenWidth / 2, screenHeight), (myMouseClick))
-
+    # Blit the screen as final part of Display
     pygame.display.flip()
+    ''' DISPLAY / OUTPUT Above '''
