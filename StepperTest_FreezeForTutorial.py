da22a27f1b17f68c4c9224fd0b670db409433982
root <root@raspberrypi.(none)>
2014-01-14 03:03:46 -0800

    Stepper Driving Working :D
    Signed-off-by: root <root@raspberrypi.(none)>

--- StepperTest_FreezeForTutorial.py
+++ StepperTest_FreezeForTutorial.py
@@ -5,30 +5,35 @@ import time
 import RPi.GPIO as GPIO
 
 
-StepPin = 18
-DirPin = 17
+StepPin = 23
+DirPin = 24
 x = 0
-pause = 0.001
+pause = 0.005  #0.001
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(StepPin, GPIO.OUT)
 GPIO.setup(DirPin, GPIO.OUT)
 
-while x <= 10:
+while x <= 5:
     print(x)
     x = x + 1
     i = 0
     j = 0
+
     time.sleep(1)
     GPIO.output(DirPin, GPIO.HIGH)
+
     while i <= 250:
         i = i + 1
         GPIO.output(StepPin, GPIO.HIGH)
         time.sleep(pause)
         GPIO.output(StepPin, GPIO.LOW)
         time.sleep(pause)
-    time.sleep(1)
+        
+    time.sleep(.5)
     GPIO.output(DirPin, GPIO.LOW)
-    while j <= 500:
+
+    while j <= 250:
         j = j + 1
         GPIO.output(StepPin, GPIO.HIGH)
         time.sleep(pause)
